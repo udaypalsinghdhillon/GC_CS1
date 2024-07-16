58fa4321601780629ab7698cd5427b91ab71b928
Udaypal Singh Dhillon <udaydoesdata@gmail.com>
2024-07-16 16:42:24 +0200

    Q5 update

--- sql_analysis/Q5.sql
+++ sql_analysis/Q5.sql
@@ -1,11 +1,10 @@
 /* 
 Question: Which are the busiest stations for booking rides used by members and non-members(casuals)?
-Note: Busiest station was defined based on number of rides (either starting or ending) from a station.
+Note: Busiest station was defined based on number of rides starting from a station.
 */
 
 -- Top 10 busiest stations for members
 
--- Ride start stations
 SELECT
     start_station_name,
     COUNT(ride_id) AS bookings
@@ -19,29 +18,11 @@ GROUP BY
 ORDER BY
     bookings DESC
 LIMIT 10;
-
-
--- Ride end stations
-SELECT
-    end_station_name,
-    COUNT(ride_id) AS bookings
-FROM 
-    annual_data
-WHERE
-    end_station_name IS NOT NULL
-    AND member_casual = 'member'
-GROUP BY
-    end_station_name
-ORDER BY
-    bookings DESC
-LIMIT 10;
-
 
 
 
 -- Top 10 busiest stations for non-members(casuals)
 
--- Ride start stations
 SELECT
     start_station_name,
     COUNT(ride_id) AS bookings
@@ -56,17 +37,3 @@ ORDER BY
     bookings DESC
 LIMIT 10;
 
--- Ride end stations
-SELECT
-    end_station_name,
-    COUNT(ride_id) AS bookings
-FROM 
-    annual_data
-WHERE
-    end_station_name IS NOT NULL
-    AND member_casual = 'casual'
-GROUP BY
-    end_station_name
-ORDER BY
-    bookings DESC
-LIMIT 10;
